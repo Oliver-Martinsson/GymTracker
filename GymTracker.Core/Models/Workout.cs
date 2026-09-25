c6655ebdc927f2b0ffc41b74a4600e3e0ae57f4d
Tobias Kickmaier <t.kickmaier@hotmail.com>
2026-09-25 11:04:36 +0200

    Corrected fields

--- GymTracker.Core/Models/Workout.cs
+++ GymTracker.Core/Models/Workout.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace GymTracker.Core.Models
@@ -8,8 +7,7 @@ namespace GymTracker.Core.Models
     public class Workout
     {
         public int Id { get; set; }
-        [Required]
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public List<Exercise> Routines { get; set; } = new();
     }
 }
