1a196b928b072943e4d65f568a65f2687476b778
Tobias Kickmaier <t.kickmaier@hotmail.com>
2026-09-25 10:55:35 +0200

    Added required field

--- GymTracker.Core/Models/Workout.cs
+++ GymTracker.Core/Models/Workout.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace GymTracker.Core.Models
@@ -7,6 +8,7 @@ namespace GymTracker.Core.Models
     public class Workout
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public List<Exercise> Routines { get; set; } = new();
     }
