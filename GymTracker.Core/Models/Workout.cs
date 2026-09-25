518b1cbd3d80fd9318f2bfd2fd63096d2ac5f185
Kickmaier <t.kickmaier@hotmail.com>
2026-09-25 10:47:32 +0200

    Update GymTracker.Core/Models/Workout.cs
    Thanks
    
    Co-authored-by: Oliver Martinsson <sweprotecters15@gmail.com>

--- GymTracker.Core/Models/Workout.cs
+++ GymTracker.Core/Models/Workout.cs
@@ -7,6 +7,7 @@ namespace GymTracker.Core.Models
     public class Workout
     {
         public int Id { get; set; }
+[required]
         public string Name { get; set; }
         public List<Exercise> Routines { get; set; } = new();
     }
