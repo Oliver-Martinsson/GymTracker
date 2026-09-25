7239942e46c5f7b7592a0f5d477eedeb25948918
Tobias Kickmaier <t.kickmaier@hotmail.com>
2026-09-25 12:45:05 +0200

    Created GymDbContext

---
+++ GymTracker.Infrastructure/Data/GymDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GymTracker.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymTracker.Infrastructure.Data
+{
+    public class GymDbContext : DbContext
+    {
+        public GymDbContext(DbContextOptions<GymDbContext> options) : base(options){ }
+
+        public DbSet<Workout> Workouts { get; set; }
+        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<WorkoutExercise> WorkoutExercises { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<WorkoutExercise>()
+                .HasOne(we => we.Workout)
+                .WithMany(w => w.WorkoutExercise)
+                .HasForeignKey(we => we.WorkoutId);
+
+            modelBuilder.Entity<WorkoutExercise>()
+                .HasOne(we => we.Exercise)
+                .WithMany(e => e.WorkoutExercises)
+                .HasForeignKey(we =>we.ExerciseId);
+        }
+    }
+}
